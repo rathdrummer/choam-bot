c46bc3cc129b41946a9572bc2e057664a89daed4
pizza <rathdrummer@gmail.com>
2021-12-02 20:18:57 +0000

    Bugfix for prev commit

--- bot.py
+++ bot.py
@@ -25,8 +25,8 @@ async def process_bot_command(message):
         await message.channel.send("**"+response+"**")
 
     elif command == "version":
-        date = os.path.getmtime("bot.py")
-        await message.channel.send("**CHOAM Listings bot** by Pizza \n *Last updated "+time.ctime(date)+"*")
+        date = os.path.getmtime("/home/adam/choam-bot/bot.py")
+        await message.channel.send("**CHOAM Listings bot** by Pizza \n*Last updated: "+time.ctime(date)+"*")
 
 @client.event
 async def on_ready():
